5804393a33faadce2efba775ec2dd635bcee0748
rosellij <johnroselli3@gmail.com>
2021-12-23 11:48:06 -0500

    push!

---
+++ js/index.js
@@ -0,0 +1,69 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    console.log("The JS is loaded!");
+
+    console.log(document.querySelectorAll(".portfoliocard")[0].getAttribute("id"));
+
+    // document.querySelectorAll(".portfoliocard").forEach(card => {
+
+    //     card.focus();
+
+    // });
+
+    //document.querySelector("#portfoliocard_1").focus();
+
+    // document.querySelectorAll(".portfoliocard").forEach(card => {
+
+    //     card.addEventListener("mouseover", () => {
+
+    //         let cardId = card.getAttribute("id");
+    //         console.log("Selected Card ID: " + String(cardId[cardId.length - 1]));
+    //         card.classList.add("cardhoverclass");
+
+    //         /* [0,1,2,3,4].forEach(idNum => {
+
+    //             console.log("ID Number for Each Other Card: " + String(idNum + 1));
+
+    //             if ((idNum + 1) !== Number(cardId[cardId.length - 1])) {
+
+    //                 otherCardId = "portfoliocard_" + String(idNum + 1);
+    //                 otherCard = document.querySelector("#" + otherCardId);
+
+    //                 console.log("Full ID for Each Other Card: " + otherCardId);
+    //                 console.log("Each Other Card Object: Below");
+    //                 console.log(otherCard);
+
+    //                 otherCard.classList.add("cardnothoverclass");
+    //             };
+
+    //         }); */
+    //     });
+
+    //         /*
+    //         if portfoliocard is hovered over
+    //         set its flex-grow to like 9
+    //         set the flex-shrink of all the other cards to like 
+    //         */
+
+    //     card.addEventListener("mouseout", () => {
+
+    //         card.classList.remove("cardhoverclass");
+
+    //         /*document.querySelectorAll(".portfoliocard").forEach(card => {
+    //             card.classList.remove("cardnothoverclass");
+
+    //             console.log("Here's " + card.getAttribute("id") + "'s object, now with cardnothoverclass removed: ");
+    //             console.log(card);
+    //         });*/
+        
+    //     });
+
+    //     card.addEventListener("click", () => {
+
+
+
+    //     });
+
+    // });
+     
+});
